1f5821ff4df524491d226d1fd8be70049239750e
Tugrul <tugrulaslan@gmail.com>
2014-12-26 00:17:03 +0200

    Second commit

---
+++ src/main/java/com/tugrulaslan/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.tugrulaslan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	   public String printHello(ModelMap model) {
+	      model.addAttribute("message", "IndexPage");
+
+	      return "index";
+	   }
+}
